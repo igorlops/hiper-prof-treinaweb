796dc62fbde1f948e979e2183884db5ff0fb80f3
igorlops <claudioigorai@gmail.com>
2024-09-25 21:35:59 -0300

    Criando interface para cadastrar professor

--- src/data/@types/professor.ts
+++ src/data/@types/professor.ts
@@ -9,3 +9,15 @@ export interface ProfessorInterface{
     created_at: string;
     updated_at: string;
 }
+
+export interface ProfessorCadastroInterface extends 
+    Omit<ProfessorInterface, 'id'|'created_at'|'updated_at'|'valor_hora'> {
+    password: string;
+    password_confirmation: string;
+    valor_hora: string|number;
+}
+
+export interface ProfessorErrorInterface extends Omit<ProfessorCadastroInterface, 'idade'|'valor_hora'> {
+    idade:string;
+    valor_hora:string;
+}
