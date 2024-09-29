e33b2f3cf0fe45fff7590b4acfd8fd4e35e60200
igorlops <claudioigorai@gmail.com>
2024-09-29 00:55:09 -0300

    adicionando profile ao header, criando funções separadas para desktop e mobile

---
+++ src/ui/components/inputs/ButtonFile/styles.tsx
@@ -0,0 +1,16 @@
+import { styled } from "@mui/material/styles";
+import { Box, Button } from "@mui/material";
+
+
+export const BoxStyled = styled(Box)`
+    position: relative;
+    z-index: 1;
+
+    input {
+        position: absolute;
+        opacity: 0;
+    }
+`
+export const ButtonFileStyled = styled(Button)`
+
+`
--- src/ui/components/surfaces/Base/index.tsx
+++ src/ui/components/surfaces/Base/index.tsx
@@ -1,13 +1,17 @@
 import Link from "@components/navigation/Link";
 import { AppBar, Box, Button, Container, Drawer, Icon, IconButton, Toolbar, useMediaQuery, useTheme } from "@mui/material";
-import { PropsWithChildren, useState } from "react";
+import { PropsWithChildren, useContext, useState } from "react";
 import { BoxDrawer, ButtonStyle } from "./styles";
-import { useRouter } from "next/router";
+import { NextRouter, useRouter } from "next/router";
 import { Router } from "@routes/routes";
+import UserHeaderMenu from "@components/navigation/UserHeaderMenu";
+import { ProfessorContext } from "@data/contexts/ProfessorContext";
+import { ProfessorInterface } from "@data/@types/professor";
+import { ApiService } from "@data/services/ApiService";
 
-export function LinkLogo() {
+export function LinkLogo({professor}:{ professor?: ProfessorInterface}) {
     return (
-        <Link href="/">
+        <Link href={professor?.id ? '/professor' : '/'}>
             <img src="logo.png" alt="" />
         </Link>
     )
@@ -16,9 +20,23 @@ export default function Base({children}:PropsWithChildren) {
     const {breakpoints} = useTheme()
     const isSmDevice = useMediaQuery(breakpoints.up("sm")),
     [isOpenDrawer, setIsOpenDrawer] = useState<boolean>(false),
-    router = useRouter()
-    function onSejaProfessor() {
-        Router.cadastroProfessor.push(router)
+    router = useRouter(),
+    {ProfessorState: professor,ProfessorDispatch} = useContext(ProfessorContext)
+
+    async function handleLogout() {
+        await ApiService.post('/api/auth/logout', {
+            refresh_token: localStorage.getItem('refresh_token_hiperprof')
+        }, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token_hiperprof')}`
+            }
+        })
+        .then(() => {
+            localStorage.removeItem('refresh_token_hiperprof')
+            localStorage.removeItem('token_hiperprof') 
+            ProfessorDispatch(undefined);
+            Router.home.push(router)
+        })
     }
 
     return (
@@ -28,37 +46,21 @@ export default function Base({children}:PropsWithChildren) {
                     <Box sx={{width:"100%", display:"flex", justifyContent:"space-between", alignItems:"center"}}>
                     {isSmDevice ? 
                         (
-                            <>
-                                <LinkLogo/>
-                                <Box sx={{display:"flex",alignItems:"center"}}>
-                                    <Link href="/" color={"inherit"}>HOME</Link>
-                                    <Link href="/login" color={"inherit"} sx={{mx:5}}>LOGIN</Link>
-                                    <ButtonStyle variant="outlined" onClick={onSejaProfessor}>Seja um PROFESSOR</ButtonStyle>
-                                </Box>
-                            </>
+                            <HeaderDesktop router={router} professor={professor} onLogout={handleLogout}/>
                         ) : (
                             <>
                                 <LinkLogo/>
                                 <IconButton 
                                     color="inherit"
                                     onClick={() => setIsOpenDrawer(true)}    
-                                >
+                                    >
                                     <Icon>menu</Icon>
                                 </IconButton>
                                 <Drawer open={isOpenDrawer}
                                     onClick={() => setIsOpenDrawer(false)}
                                     onClose={() => setIsOpenDrawer(false)}
                                 >
-                                    <BoxDrawer>
-                                        <div className="linkImage">
-                                            <LinkLogo/>
-                                        </div>
-                                        <Box sx={{display:"flex",flexDirection:"column",alignItems:"center", mx:5}}>
-                                            <Link href="/">HOME</Link>
-                                            <Link href="/login"  sx={{my:2}}>LOGIN</Link>
-                                            <ButtonStyle variant="outlined">Seja um PROFESSOR</ButtonStyle>
-                                        </Box>
-                                    </BoxDrawer>
+                                    <HeaderMobile professor={professor} onLogout={handleLogout} />
                                 </Drawer>
                             </>
                         )
@@ -68,5 +70,85 @@ export default function Base({children}:PropsWithChildren) {
             </AppBar>
             <Container component={"main"}>{children}</Container>
         </Box>
+    )
+}
+
+interface HeaderDesktopProps {
+    router:NextRouter;
+    professor?: ProfessorInterface;
+    onLogout?: () => void
+}
+
+function HeaderDesktop({router, professor, onLogout}:HeaderDesktopProps) {
+    function onSejaProfessor() {
+        Router.cadastroProfessor.push(router)
+    }
+
+    const [openMenu, setOpenMenu] = useState(false);
+
+
+    return (
+        <>
+            <LinkLogo professor={professor}/>
+            <Box sx={{display:"flex",alignItems:"center"}}>
+                {professor?.id ? (
+                    <>
+                        <Link href="/professor" color={"inherit"} sx={{ mx: 2}}>
+                            Lista de alunos
+                        </Link>
+                        <UserHeaderMenu
+                            isMenuOpen={openMenu}
+                            onMenuClick={() => setOpenMenu(false)}
+                            onMenuClose={() => setOpenMenu(false)}
+                            onClick={() => setOpenMenu(true)}
+                            handleLogout={() => onLogout}
+                        />
+                    </>
+                ) : (
+                    <>
+                        <Link href="/" color={"inherit"}>HOME</Link>
+                        <Link href="/login" color={"inherit"} sx={{mx:5}}>LOGIN</Link>
+                        <ButtonStyle variant="outlined" onClick={onSejaProfessor}>Seja um PROFESSOR</ButtonStyle>
+                    </>
+                )}
+            </Box>
+        </>
+    );
+}
+
+interface HeaderMobileProps {
+    professor?: ProfessorInterface;
+    onLogout?: () => void
+}
+
+function HeaderMobile({professor, onLogout}: HeaderMobileProps) {
+    return (
+        <>
+            <BoxDrawer>
+                <div className="linkImage">
+                    <LinkLogo/>
+                </div>
+                {professor?.id ? (
+                    <MenuListDrawerLinks>
+                        <Link href="/professor">Lista de alunos</Link>
+                        <Link href="/professor/cadastro-professor"  sx={{my:2}}>Cadastro Professor</Link>
+                        <Link href="/" onClick={onLogout}>Logout</Link>
+                    </MenuListDrawerLinks>
+            ) : (
+                <MenuListDrawerLinks>
+                    <Link href="/">HOME</Link>
+                    <Link href="/login"  sx={{my:2}}>LOGIN</Link>
+                    <ButtonStyle variant="outlined">Seja um PROFESSOR</ButtonStyle>    
+                </MenuListDrawerLinks>
+            )}
+            </BoxDrawer>
+            
+        </>
+    )
+}
+
+function MenuListDrawerLinks({children} : PropsWithChildren) {
+    return (
+        <Box sx={{display:"flex",flexDirection:"column",alignItems:"center", mx:5}}>{children}</Box>
     )
 }
