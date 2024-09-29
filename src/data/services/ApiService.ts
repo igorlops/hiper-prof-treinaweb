7a39a0a391dcb49e6583aff9950c0d746e9a609f
igorlops <claudioigorai@gmail.com>
2024-09-29 00:52:37 -0300

    criação do método de refresh token, passando o interceptors, para verificar o token das requisições

--- src/data/services/ApiService.ts
+++ src/data/services/ApiService.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 
 export const ApiService = axios.create({
     baseURL: "https://alunos.treinaweb.com.br/hyperprof",
@@ -7,3 +7,36 @@ export const ApiService = axios.create({
         "Accept-Language" :"pt-BR"
     }
 })
+
+ApiService.interceptors.response.use((response) => {
+    return response;
+}, async (error: AxiosError) => {
+    if(error.request.status === 401) {
+        return await handleTokenRefresh(error)
+    }
+    return error;
+})
+
+async function handleTokenRefresh(error: AxiosError) {
+    try {
+        const refreshToken = localStorage.getItem('refresh_token_hiperprof');
+        await ApiService.post<{token: string, refresh_token: string}>
+            ('/api/auth/refesh', { 
+                refresh_token: refreshToken
+            })
+            .then(({data}) => {
+                localStorage.setItem('token_hiperprof',data.token);
+                localStorage.setItem('refresh_token_hiperprof',data.refresh_token);
+            });
+
+            return await ApiService({
+                ...error.config,
+                headers: {
+                    ...error.config?.headers,
+                    Authorization: `Bearer ${localStorage.getItem('token_hiperprof')}`,
+                }
+            })
+    } catch(error) {
+        return Promise.reject(error)
+    }
+}
